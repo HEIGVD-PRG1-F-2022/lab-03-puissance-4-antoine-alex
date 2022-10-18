d72ee971bf1399059cce6f7bc9d67feb6ab8fa00
A2va <49582555+A2va@users.noreply.github.com>
2022-10-18 16:11:16 +0200

    Display vector
    Add Wall
    Change gitignore

--- main.cpp
+++ main.cpp
@@ -1,29 +1,34 @@
 #include <iostream>
 #include <cstdlib>
-#include <array>
+#include <vector>
 
 using namespace std;
+using Plateau = vector<vector<int>>;
+
+
+void affichageConsole(Plateau plateau);
 
 int main() {
     cout << "Regles de la puissance 4 : " << endl;
 
-         const int longueur = 6;
-         const int hauteur = 7;
+    const int longueur = 7;
+    const int hauteur = 6;
 
-array <array <int, hauteur> ,longueur> plateau;
-
-
-
-
-
-
-
+    Plateau plateau(hauteur, std::vector<int>(longueur, 0));
+    affichageConsole(plateau);
 
 
     return EXIT_SUCCESS;
 }
 
-/*void affichageConsole(array <array <int, hauteur> ,longueur> plateau){
 
 
-}*/
+void affichageConsole(Plateau plateau) {
+    vector<int> colonne = plateau[0];
+    for(vector<int> colonne: plateau) {
+        for(int i : colonne) {
+            std::cout << i;
+        }
+        std::cout << std::endl;
+    }
+}
