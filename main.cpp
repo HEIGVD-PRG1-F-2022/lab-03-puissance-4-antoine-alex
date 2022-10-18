c427173ef6e3bbc2199c9f6b595248cb7fba8302
alexb <alexander.bouriot@heig-vd.ch>
2022-10-18 15:55:18 +0200

    Debut labo Tableau

--- main.cpp
+++ main.cpp
@@ -1,1 +1,29 @@
 #include <iostream>
+#include <cstdlib>
+#include <array>
+
+using namespace std;
+
+int main() {
+    cout << "Regles de la puissance 4 : " << endl;
+
+         const int longueur = 6;
+         const int hauteur = 7;
+
+array <array <int, hauteur> ,longueur> plateau;
+
+
+
+
+
+
+
+
+
+    return EXIT_SUCCESS;
+}
+
+/*void affichageConsole(array <array <int, hauteur> ,longueur> plateau){
+
+
+}*/
