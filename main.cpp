8d875a1d550bb221e48bcef0610c941cb214e40e
A2va <49582555+A2va@users.noreply.github.com>
2022-10-19 17:12:58 +0200

    Fix

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using Plateau = vector<vector<int>>;
 
 
 void affichageConsole(Plateau plateau);
-void verifyInput(Plateau plateau, int x, int y);
+bool verifyInput(Plateau& plateau, unsigned int x, int joueur);
 
 int main() {
     cout << "Regles de la puissance 4 : " << endl;
@@ -16,18 +16,21 @@ int main() {
     const int hauteur = 6;
 
     Plateau plateau(hauteur, std::vector<int>(longueur, 0));
+    plateau[5][0] = 1;
+    verifyInput(plateau, 0,1);
     affichageConsole(plateau);
 
 
     return EXIT_SUCCESS;
 }
 
-bool verifyInput(Plateau plateau, unsigned int x, int joueur) {
+bool verifyInput(Plateau& plateau, unsigned int x, int joueur) {
     for(int i = plateau[0].size() - 1; i > 0; i--) {
-        if(plateau[x][i] != 0) {
-            plateau[x][i] = joueur;
-            return true;
-        }
+        // if(plateau[x][i] != 0) {
+        //     plateau[x][i] = joueur;
+        //     return true;
+        // }
+        std::cout << i;
     }
     return false;
 }
